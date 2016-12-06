87da12cd373bd7d3024f88b0e2426495f450202b
n-gondo123 <n.gondo123@gmail.com>
2016-12-06 20:47:19 +0900

    schema test

--- todo-api/test.js
+++ todo-api/test.js
@@ -1,12 +1,20 @@
 var dt = require('dredd-transactions');
+var drafter = require('drafter.js');
 var fs = require('fs');
+var util = require('util');
 
 fs.readFile("./api-blueprint/todo-api.md", function(err, data) {
   // console.log(Buffer(data).toString());
   var doc = Buffer(data).toString();
-  dt.compile(doc, './api-blueprint/todo-api.md', function(err, compilationResult) {
-    compilationResult.transactions.forEach(function(transaction) {
-      console.log(transaction);
-    });
-  });
+  // dt.compile(doc, './api-blueprint/todo-api.md', function(err, compilationResult) {
+  //   compilationResult.transactions.forEach(function(transaction) {
+  //     console.log(transaction);
+  //   });
+  // });
+  var options = {
+    type: 'ast'
+  };
+  drafter.parse(doc, options, function(err, result) {
+    console.log(util.inspect(result, false, null));
+  })
 });
